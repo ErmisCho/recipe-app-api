0efefd2fee0cd95d514dc6286d9d22cb72332fab
Ermis <ermis.chorinopoulos@gmail.com>
2023-04-07 19:41:14 +0200

    feat: implemented custom command 'wait_for_db'

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,6 +2,12 @@
 Django commadn to wait for the database to be available
 """
 
+import time
+
+from psycopg2 import OperationalError as Psycopgs2pError
+
+# error Django throws when db is not ready
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
@@ -10,4 +16,19 @@ class Command(BaseCommand):
 
     # Unnamed values go in *args. Keyword arguments go in **options
     def handle(self, *args, **options):
-        pass
+        """Entrypoint for command"""
+
+        # write that on the command line
+        self.stdout.write('Waiting for database...')
+        db_up = False
+
+        while not db_up:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopgs2pError, OperationalError):
+                self.stdout.write(
+                    "Database unavailable, waiting for 1 second...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
