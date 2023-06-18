385628a23d56f381ca9b2f7cfbe78bf67d8e447b
Ermis <ermis.chorinopoulos@gmail.com>
2023-06-18 19:09:59 +0200

    feat[security]: the Django key is changed and not anymore publicly available

--- app/app/settings.py
+++ app/app/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+from decouple import config
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-otx9=1yifo#)x(cgaoj0b2re$x(t%l%cb3s7&qa+9_z@mk6l_r'
+# SECRET_KEY = '--secret-key--'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
