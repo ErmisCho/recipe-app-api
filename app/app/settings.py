ba233d7639599456682cc3a4b6726ce9708d1499
Ermis <ermis.chorinopoulos@gmail.com>
2023-06-18 23:00:10 +0200

    feat: the django secret key is imported as per environment file

--- app/app/settings.py
+++ app/app/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from decouple import config
 import os
 from pathlib import Path
-
-from decouple import config
+# env_name = os.getenv('ENV_NAME', 'dev')
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,8 +24,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '--secret-key--'
-SECRET_KEY = config("SECRET_KEY", default="")
+SECRET_KEY = '--secret-key-to-be-replaced--'
+
+if os.path.exists("settings/dev.py"):
+    from settings.prod import *
+elif os.path.exists("settings/github.py"):
+    from settings.prod import *
+elif os.path.exists("settings/prod.py"):
+    from settings.prod import *
+
+# ENVIRONMENT_TYPE = config("ENVIRONMENT_TYPE")
+# SECRET_KEY = config("SECRET_KEY", default="")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ app/app/settings/github.py
@@ -0,0 +1,1 @@
+SECRET_KEY = "be^(7!p2)88(vrx70#i=4^y2&kz9gjfktkl*1&wh95ydfo466)"
