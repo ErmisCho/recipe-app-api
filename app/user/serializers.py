84a59ad496bb0b01a3a74847f53bc47cad068519
Ermis <ermis.chorinopoulos@gmail.com>
2024-05-31 23:11:25 +0200

    fix: token API tests all pass now

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,9 +1,10 @@
 """
 Serializers for the user API View.
 """
-
-from django.contrib.auth import (get_user_model, authenticate,)
-
+from django.contrib.auth import (
+    get_user_model,
+    authenticate,
+)
 from django.utils.translation import gettext as _
 
 from rest_framework import serializers
@@ -14,13 +15,8 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ['email', 'password', 'name']
-        extra_kwargs = {
-            'password': {
-                'write_only': True,
-                'min_length': 5
-            }
-        }
+        fields = ('email', 'password', 'name')
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, validated_data):
         """Create and return a user with encrypted password."""
--- app/user/urls.py
+++ app/user/urls.py
@@ -9,5 +9,5 @@ app_name = 'user'  # used for the reverse mapping in the file "test_user_api.py"
 urlpatterns = [
     # used for the reverse mapping in the file "test_user_api.py"
     path('create/', views.CreateUserView.as_view(), name='create'),
-    path('token/', views.CreateUserView.as_view(), name='token'),
+    path('token/', views.CreateTokenView.as_view(), name='token'),
 ]
